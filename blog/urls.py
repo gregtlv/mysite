5da2b94c9973bf3079437d64bb9d4237276edb17
manov@manov.ru <gregory.manov@gmail.com>
2017-12-21 15:25:10 +0200

    Whats going on

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_last, name='post_last')
+    url(r'^$', views.post_last, name='post_last') # it is function name in views.py
 ]
