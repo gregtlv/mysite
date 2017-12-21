c0c9d1da8e28bf96a8c5248c696c2db77b766b14
manov@manov.ru <gregory.manov@gmail.com>
2017-12-21 12:07:45 +0200

    First template added.

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # path('blog/', include('blog.urls')),
+    path(r'', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
